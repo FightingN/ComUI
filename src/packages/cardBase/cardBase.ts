e0fcb1261e60b1a752e62c9a8dbf9cc2e04c50d9
FightingN <18300240052@163.com>
2021-06-30 10:22:48 +0800

    组件首页完成

--- src/packages/cardBase/cardBase.ts
+++ src/packages/cardBase/cardBase.ts
@@ -1,11 +1,33 @@
-Page({
-  data: {
-    name: "cardBase"
+Component({
+  properties: {
+    baseList: {
+      type: Array,
+      value: [
+        {
+          title: '基础',
+          subhead: '包含颜色、文本、图标',
+          id: '1',
+          imgUrl: '/assets/ComUI/base/jichu.png',
+          pageUrl: '/subpackages/module/pages/base/base',
+          iconUrl: '/assets/ComUI/base/right-circle.png',
+        },
+        {
+          title: '视图',
+          subhead: '包含通告栏、标签、徽章等',
+          id: '2',
+          imgUrl: '../../assets/ComUI/base/shitu.png',
+          pageUrl: '/subpackages/module/pages/base/base',
+          iconUrl: '/assets/ComUI/base/right-circle.png',
+        },
+      ],
+    },
   },
-  onLoad () {
-
+  methods: {
+    onTapRedirect(e: any) {
+      const { pageurl } = e.currentTarget.dataset
+      wx.navigateTo({
+        url: pageurl,
+      })
+    },
   },
-  onShow () {
-
-  }
 })
--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,8 +1,63 @@
 Page({
-  data: {},
-  onRedirect() {
-    wx.navigateTo({
-      url: '/subpackages/test/pages/test/test',
-    })
+  data: {
+    baseList: [
+      {
+        title: '基础',
+        subhead: '包含颜色、文本、图标',
+        id: '1',
+        imgUrl: '/assets/ComUI/base/jichu.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+      {
+        title: '视图',
+        subhead: '包含通告栏、标签、徽章等',
+        id: '2',
+        imgUrl: '/assets/ComUI/base/shitu.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+      {
+        title: '操作反馈',
+        subhead: '包含对话框、进度条、动作面板等',
+        id: '3',
+        imgUrl: '/assets/ComUI/base/caozuo.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+      {
+        title: '表单',
+        subhead: '包含输入框、单选框、复选框等',
+        id: '4',
+        imgUrl: '/assets/ComUI/base/biaodan.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+      {
+        title: '布局',
+        subhead: '包含列表、浮层、卡片等',
+        id: '5',
+        imgUrl: '/assets/ComUI/base/buju.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+      {
+        title: '导航',
+        subhead: '包含标签栏、导航栏、分段器等',
+        id: '6',
+        imgUrl: '/assets/ComUI/base/daohang.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+      {
+        title: '高阶组件',
+        subhead: '包含日历等',
+        id: '7',
+        imgUrl: '/assets/ComUI/base/query.png',
+        pageUrl: '/subpackages/module/pages/base/base',
+        iconUrl: '/assets/ComUI/base/right-circle.png',
+      },
+    ],
   },
+  onLoad: function () {},
 })
